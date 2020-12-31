05dccd0fe77aaa9677155bd46ea7c8a33c26b8a3
Felix Castro <felixgonzalocastro@gmail.com>
2020-12-30 19:39:21 -0500

    routes added

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,10 +1,19 @@
 import { Fragment } from "react";
-
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom"
 
 const Routes = () => {
   return (
     <Fragment>
-      rutas
+      <Router>
+        <Switch>
+          <Route path="/admin.personas">
+            Administración personas
+          </Route>
+          <Route path="/">
+            Principal
+          </Route>
+        </Switch>
+      </Router>
     </Fragment>
   );
 }

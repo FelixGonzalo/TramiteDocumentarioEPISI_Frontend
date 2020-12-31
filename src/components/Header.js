4c4ce7bf62f8c6cbc76a439386b184756e0fedd8
Felix Castro <felixgonzalocastro@gmail.com>
2020-12-30 20:12:49 -0500

    information header added

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import './header.css'
+import logoUns from './img/logo_uns.png'
+import logoEpisi from './img/logo_episi.jpg'
+
+const Header = () => {
+  return (
+    <header class="header">
+      <div className="header-information">
+        <img src={logoUns} alt="logo UNS" height= "30px"/>
+        <h1 className="header-title">Sistema de Trámite Documentario de EPISI</h1>
+        <img src={logoEpisi} alt="logo EPISI" height= "30px"/>
+      </div>
+    </header>
+  );
+}
+ 
+export default Header;
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom"
+import Header from './Header'
 
 const Routes = () => {
   return (
     <Fragment>
       <Router>
+        <Header/>
         <Switch>
           <Route path="/admin.personas">
             Administración personas

287cc463a754887e353be3b68b2e0369973e3304
Felix Castro <felixgonzalocastro@gmail.com>
2020-12-30 22:15:14 -0500

    people list added

--- src/components/AdminPerson.js
+++ src/components/AdminPerson.js
@@ -1,24 +1,49 @@
+import { useEffect, useState } from "react"
 import './adminPerson.css'
 
 const AdminPerson = () => {
+
+  const [personas, setPersonas] = useState([])
+
+  useEffect(() => {
+    getDatos()
+  }, [])
+  
+  const getDatos = async () => {
+    const data = await fetch('http://localhost:8090/api/personas')
+    const response = await data.json()
+    setPersonas(response)
+  }
+
   return (
     <main className="container-main">
       <h2 className="default-title">Administrador de Personas</h2>
       <div className="table-responsive">
         <table className="adminPerson-list">
           <thead>
-            <th>#</th>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Puesto</th>
-          </thead>
-          <tbody id="listaPersonas_item">
             <tr>
-              <td>#</td>
-              <td>Nombre</td>
-              <td>Correo</td>
-              <td>Puesto</td>
+              <th>#</th>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Puesto</th>
             </tr>
+          </thead>
+          <tbody>
+            {
+              personas.length > 0 ? 
+                personas.map((item, index) => (
+                  <tr key={item.id}>
+                    <td>{index+1}</td>
+                    <td>{item.nombre}</td>
+                    <td>{item.correo}</td>
+                    <td>Puesto</td>
+                  </tr>
+                )) : (
+                  <tr>
+                    <td colSpan="4">Datos no disponibles</td>
+                  </tr>
+                )
+            }
           </tbody>
         </table>
       </div>

9943a392e7e053b326ab04e3a057d16ead05e355
Felix Castro <felixgonzalocastro@gmail.com>
2021-01-01 22:46:25 -0500

    validation for registration of people

--- src/components/FormPerson.js
+++ src/components/FormPerson.js
@@ -40,54 +40,70 @@ const FormPerson = () => {
           <input
             type="text"
             name="nombre"
-            placeholder={errors?.nombre?.message}
             className="input-default"
             ref={
               register({
-                required : {value: true, message: 'Nombre obligatorio'}
+                required : {value: true, message: 'nombre obligatorio'},
+                pattern : {value: /^[A-Za-zÀ-ÿ\s]+$/i, message: 'nombre no válido' }
               })
             }
           />
         </label>
+        <span class="input-error">
+          {errors?.nombre?.message}
+        </span>
         <label className="label-default"> DNI/RUC
           <input
             type="text"
             name="dniOruc"
-            placeholder={errors?.dniOruc?.message}
             className="input-default"
             ref={
               register({
-                required : {value: true, message: 'DNI/RUC obligatorio'}
+                required : {value: true, message: 'DNI/RUC obligatorio'},
+                pattern : {value: /[0-9]+$/, message: 'Solo números' },
+                minLength : {value: 8, message: 'muy corto, son 8 caracteres para DNI'},
+                maxLength : {value: 11, message: 'te pasaste, solo 11 caracteres para RUC'}
               })
             }
           />
         </label>
+        <span class="input-error">
+          {errors?.dniOruc?.message}
+        </span>
         <label className="label-default"> Correo
           <input
             type="text"
             name="correo"
-            placeholder={errors?.correo?.message}
             className="input-default"
             ref={
               register({
-                required : {value: true, message: 'Correo obligatorio'}
+                required : {value: true, message: 'Correo obligatorio'},
+                pattern : {value: /[a-z0-9_.-]+@[a-z]+\.[a-z0-9_.-]+[a-z0-9]/i, message: 'correo no válido' }
               })
             }
           />
         </label>
+        <span class="input-error">
+          {errors?.correo?.message}
+        </span>
         <label className="label-default"> Cod. estudiante
           <input
             type="text"
             name="codigoEstudiante"
-            placeholder={errors?.codigoEstudiante?.message}
             className="input-default"
             ref={
               register({
-                required : {value: true, message: 'Código de estudiante obligatorio'}
+                required : {value: true, message: 'código de estudiante obligatorio'},
+                pattern : {value: /[0-9]+$/, message: 'Solo números' },
+                minLength : {value: 10, message: 'muy corto, son 10 caracteres'},
+                maxLength : {value: 10, message: 'te pasaste, solo 10 caracteres'}
               })
             }
           />
         </label>
+        <span class="input-error">
+          {errors?.codigoEstudiante?.message}
+        </span>
         <label className="label-default"> Puesto
           <select 
             name="puesto" 

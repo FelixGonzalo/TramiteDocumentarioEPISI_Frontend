a0b9944688e914dd25178ed20aef2be4977fd86a
Felix Castro <felixgonzalocastro@gmail.com>
2021-01-18 21:51:29 -0500

    solucion de lista null  en registrar tramite

--- src/components/TramiteForm.js
+++ src/components/TramiteForm.js
@@ -106,9 +106,10 @@ const TramiteForm = () => {
              ref={register()}
           >
           {
+            tiposTramite.length > 0 ? 
             tiposTramite.map((item) => (
               <option key={item.id} value={item.id}>{item.nombre}</option>
-            ))
+            )) : (<option></option>)
           }
           </select>
         </label>

aadd967ed66a1ba868868bf5e63c1852cd307331
Felix Castro <felixgonzalocastro@gmail.com>
2021-01-23 10:26:55 -0500

    postArchivo en archivoDuck

--- src/redux/archivoDucks.js
+++ src/redux/archivoDucks.js
@@ -31,3 +31,31 @@ export const getArchivos = () => async (dispatch, getState) => {
     alert.alertError(error)
   }
 }
+
+export const postArchivo = (archivo, event) => async (dispatch, getState) => {
+  try {
+    var myjson = {
+      "descripcion": archivo.descripcion,
+      "tipoArchivo": {
+          "id": Number(archivo.id),
+      }
+    }
+    const response = await fetch('http://localhost:8090/api/archivos', {
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(myjson)
+    })
+    const data = await response.json()
+    if(data.status === 500) {
+      alert.alertError(`${data.status}: ${data.error}`)
+    } else {
+      alert.alertOk(`${data.nombre} registrado !!`)
+      event.target.reset()
+    }
+  } catch (error) {
+    console.log(error)
+    alert.alertError(error)
+  }
+}
